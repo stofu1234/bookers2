ef11ab03a9deeda627682b7d967e8bfe9fd5402f
高崎 直正 <yamaboku@gmail.com>
2021-08-29 20:56:46 +0000

    Postimage追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_062157) do
+ActiveRecord::Schema.define(version: 2021_08_29_204222) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
